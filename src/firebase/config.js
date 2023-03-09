024cbc297ddf72837ac00a4c7af824f9e4e624f4
rex <youngox9@gmail.com>
2023-03-09 09:26:10 +0800

    add

---
+++ src/firebase/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  apiKey: "AIzaSyDkkQUgZazZr1vvhJ3qN0VuU_W8mj6maYs",
+  authDomain: "rex-test-93247.firebaseapp.com",
+  databaseURL: "https://rex-test-93247.firebaseio.com",
+  projectId: "rex-test-93247",
+};
