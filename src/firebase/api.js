084b3dbace5478108d0405c12cedeabf63e54b8d
rex <youngox9@gmail.com>
2023-03-10 11:34:20 +0800

    test

--- src/firebase/api.js
+++ src/firebase/api.js
@@ -1,23 +1,23 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-import { list } from 'rxfire/database';
-import { authState } from 'rxfire/auth';
-import { map } from 'rxjs/operators';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import { list } from "rxfire/database";
+import { authState } from "rxfire/auth";
+import { map } from "rxjs/operators";
 
-import { config } from './config';
+import { config } from "./config";
 
 const app = firebase.initializeApp(config);
 const database = firebase.database();
 const auth = firebase.auth();
-const todolistRef = database.ref('todolist');
+const todolistRef = database.ref("todolist");
 
 const getUid = () => auth.currentUser.uid;
 
 export const login = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({
-    prompt: 'select_account'
+    prompt: "select_account",
   });
   auth.signInWithRedirect(provider);
 };
@@ -28,41 +28,35 @@ export const logout = () =>
   auth
     .signOut()
     .then(() => {
-      console.log('logout success!');
+      console.log("logout success!");
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
     });
 
 export const getTodo = () => {
   return list(todolistRef.child(getUid())).pipe(
-    map(changes =>
-      changes.map(c => {
+    map((changes) =>
+      changes.map((c) => {
         return { key: c.snapshot.key, ...c.snapshot.val() };
       })
     )
   );
 };
 
-export const addTodo = name => {
+export const addTodo = (name) => {
   todolistRef.child(getUid()).push({
     name,
-    isFinished: false
+    isFinished: false,
   });
 };
 
-export const removeTodo = key => {
-  todolistRef
-    .child(getUid())
-    .child(key)
-    .remove();
+export const removeTodo = (key) => {
+  todolistRef.child(getUid()).child(key).remove();
 };
 
 export const updateTodo = (key, status) => {
-  todolistRef
-    .child(getUid())
-    .child(key)
-    .update({
-      isFinished: status
-    });
+  todolistRef.child(getUid()).child(key).update({
+    isFinished: status,
+  });
 };
